22afaceef2f2ec1b1619dde0509190eca31dc6d6
AdrianRC <adrian.rosario.codik@gmail.com>
2020-08-24 06:23:13 -0400

    style(index): make it a bit less awful

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
+  <div class="space-y-6">
     <img src="@/assets/skater.jpg" alt="skater standing showing their socks" />
+    <img
+      class="lg:hidden"
+      src="@/assets/skater.jpg"
+      alt="skater standing showing their socks"
+    />
+    <img
+      class="sm:hidden"
+      src="@/assets/skater.jpg"
+      alt="skater standing showing their socks"
+    />
   </div>
 </template>
 
